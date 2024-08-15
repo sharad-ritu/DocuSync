683d928d7e224d17c7661cd65abd0382c9cf5d82
Sharad <sharadritu.58.69@gmail.com>
2024-08-15 22:06:31 +0545

    setup editor and clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter as FontSans } from "next/font/google"
 import './globals.css'
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -16,7 +18,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: '#3371ff',
+          fontSize: '16px',
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen font-sans antialiased",
@@ -24,7 +35,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         )}
       >
         { children }
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
